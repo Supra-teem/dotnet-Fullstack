3f071dad15581ab341d887cf372afacd8c391f13
Supra-teem <abir.bose14@gmail.com>
2021-08-31 21:28:26 +0530

    wpf form 2

---
+++ WPF/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //val(textbox1.Text=a;
+            textbox3.Text = (Convert.ToInt32(textbox1.Text) + Convert.ToInt32(textbox2.Text)).ToString();
+
+           
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPF/WpfApp2/circle diameter/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace circle_diameter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string dia = (2 * Convert.ToInt32(tb1.Text)).ToString();
+            string circum = (2 * 3.141 * Convert.ToInt32(tb1.Text)).ToString();
+            MessageBox.Show(dia,"This is Diameter");
+            MessageBox.Show(circum,"Circumference");
+        }
+    }
+}
