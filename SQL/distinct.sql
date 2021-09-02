e9bc190e1610b856f49964e52198db3c897d1292
Supra-teem <abir.bose14@gmail.com>
2021-09-02 17:51:50 +0530

    adding subquery

--- SQL/distinct.sql
+++ SQL/distinct.sql
@@ -6,4 +6,4 @@ create table test_distinct
 )
 insert into test_distinct values('Suresh',45),('Suresh',46),('Suresh',47),('Nidhi',46),('Shyamal',46),('Sujoy',45),('Bimal',47)
 select * from test_distinct
-select count(*) as Total_age from (select distinct age, name from test_distinct) as t
+select count(*) as Total_age from (select distinct age from test_distinct) as t
