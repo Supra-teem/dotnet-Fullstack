a0452be83d86ca1e5e3a90e2945966d971ab535c
Supra-teem <abir.bose14@gmail.com>
2021-09-03 12:52:33 +0530

    modified functions in sql

--- SQL/functions.sql
+++ SQL/functions.sql
@@ -1,4 +1,20 @@
+--table valued functions
 create function stu_join()
 returns table as return (select stu.roll,stu.name,stu.age,fees.course,fees.amount_paid from stu left join fees on stu.roll=fees.roll)
 
 select * from stu_join()
+
+--scalar functions
+create function my_function
+(
+	@my_age int,
+	@my_marks int
+
+)
+returns nvarchar(30)
+as
+begin return (select @my_age+' '+@my_marks)
+end
+
+select * from student
+select dbo.my_function(age,marks) as Details from student
