0a01e8fca890c48b5d29199048c218fe36ac0c4d
Supra-teem <abir.bose14@gmail.com>
2021-09-07 15:09:51 +0530

    using navbar

---
+++ ASP.NET MVC/Day2/hyperlink/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hyperlink.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
