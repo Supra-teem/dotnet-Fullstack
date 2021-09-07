c843219df16d50ab4265a4908ff076b2cf1a94ad
Supra-teem <abir.bose14@gmail.com>
2021-09-07 13:12:38 +0530

    html controls & strongly typed controls

---
+++ ASP.NET MVC/Day2/stronglytyped/Models/Employees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace stronglytyped.Models
+{
+    public class Employees
+    {
+        public int empid { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public city city { get; set; }
+        public string address { get; set; }
+        public string password { get; set; }
+
+        public dept dept { get; set; }
+
+    }
+}
+
+public enum city
+{
+    Delhi,
+    banglore,
+    kolkata,
+    chennai,
+    lucknow
+}
+public enum dept
+{
+    HR,
+    IT,
+    Admin,
+    Finance,
+    Manager
+}
