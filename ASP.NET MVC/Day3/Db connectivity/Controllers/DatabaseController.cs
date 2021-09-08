0063d366667fcdb83adaf7fed853731019dace06
Supra-teem <abir.bose14@gmail.com>
2021-09-08 11:34:50 +0530

    db connectivity

---
+++ ASP.NET MVC/Day3/Db connectivity/Controllers/DatabaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Db_connectivity.Models;
+
+namespace Db_connectivity.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        public ActionResult Index()
+        {
+            MindteckEntities db = new MindteckEntities();
+            List<Emp> em = db.Emps.ToList();
+            return View(em);
+        }
+    }
+}
