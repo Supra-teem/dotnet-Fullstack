309c9dfc339c9e4abd5cb3c8add9a6f0116971e6
Supra-teem <abir.bose14@gmail.com>
2021-09-09 16:53:17 +0530

    added action result and many more...

---
+++ ASP.NET MVC/Day4/scaffolding/Controllers/fileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace scaffolding.Controllers
+{
+    public class fileController : Controller
+    {
+        // GET: file
+        public ActionResult Index()
+        {
+            return View();
+        }
+       [HttpGet]
+       public FileResult Download()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(@"C:\Users\Suprateem\Desktop\Hello.txt");
+            string filename = "mytxt1.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filename);
+        }
+    }
+}
