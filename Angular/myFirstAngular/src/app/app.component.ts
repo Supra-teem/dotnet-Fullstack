a71fdd53f4ab8fd3bb1f0ac9c226ea6f65556845
Supra-teem <abir.bose14@gmail.com>
2021-09-15 10:21:07 +0530

    binding in angular

--- Angular/myFirstAngular/src/app/app.component.ts
+++ Angular/myFirstAngular/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`<div><h1>Hello !!!!!! {{pageHeader}}</h1>
-    <app-employee></app-employee>
-    </div>`
+  template:`<div><h1>Hello Welcome <br/>{{"First Name: " +first_name}} <br/> {{"Last Name: "+last_name}}</h1>
+  </div>`
+  //<app-employee></app-employee>
 
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageHeader:string ="Employee Details";
+  first_name:string ="Suprateem";
+  last_name: string = "Bose"
 }
