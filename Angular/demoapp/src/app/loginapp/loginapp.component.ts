8e53405e9cdb06d2165ab9cb8ba710793de16596
Supra-teem <abir.bose14@gmail.com>
2021-10-27 18:53:58 +0530

    angular practices

---
+++ Angular/demoapp/src/app/loginapp/loginapp.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-loginapp',
+  templateUrl: './loginapp.component.html',
+  styleUrls: ['./loginapp.component.scss']
+})
+export class LoginappComponent implements OnInit {
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
+
+}
