afb648f4efe3f78bf845001fbcd241b5f123cc6c
Supra-teem <abir.bose14@gmail.com>
2021-11-01 18:28:16 +0530

    way to create dashboard

--- Angular/demoapp/src/app/app.module.ts
+++ Angular/demoapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule, MdbBtnDirective, MdbCardBodyComponent, MdbCardComponent } from 'angular-bootstrap-md';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
@@ -14,7 +14,8 @@ import { LoginappComponent } from './loginapp/loginapp.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
